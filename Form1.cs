2c1e76c86d127c5a7e108fea6dd947da78a5ca7e
clar1k <sergey.hara1@gmail.com>
2025-05-13 02:43:51 +0300

    fix: correct property name for selected item in phone number validation

--- Form1.cs
+++ Form1.cs
@@ -385,7 +385,7 @@ namespace hack
             
             var operatorControl = Controls["cboOperator"] as ComboBox;
 
-            return operators.ValidatePhoneNumber(phoneNumber, operatorControl.selectedItem);
+            return operators.ValidatePhoneNumber(phoneNumber, operatorControl.SelectedItem);
         }
 
         private void HandleOperatorSubmit(object? sender, EventArgs e)
